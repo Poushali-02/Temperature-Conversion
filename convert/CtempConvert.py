93c4fa2835e8da095f9de746fa9d80eab87c6534
Poushali Bhattacharyya <poushalibhattacharyya02@gmail.com>
2024-12-06 22:24:36 +0530

    temp conversion

---
+++ convert/CtempConvert.py
@@ -0,0 +1,4 @@
+#Temperature converter -> Celcius to Farenheit
+tempC = int (input("Enter temperature in Celcius unit: "))#taking input from user
+tempF = (9 * tempC) / 5 + 32 #arithmetic operators used
+print("The temperature in Farenheit is:", tempF) #printing the value
---
+++ convert/FtempConvert.py
@@ -0,0 +1,5 @@
+#Temperature converter -> Farenheit to Celcius
+tempF = float (input("Enter temperature in Farenheit unit: "))#taking input from user
+tempF=tempF-32
+tempC = (5/9) * tempF #arithmetic operators used
+print("The temperature in Celcius is:", tempF) #printing the value
